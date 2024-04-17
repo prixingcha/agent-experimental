c49084bd6c3e18ef9f81575ffb96321ec7d6b597
prixingcha <pratik.np@gmail.com>
2024-04-16 21:26:34 -0400

    working again after revert with 2 stocks now

--- src/financial_analyst_crew/main.py
+++ src/financial_analyst_crew/main.py
@@ -1,19 +1,13 @@
-# import sys
-# sys.path.append('src')
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
 
 from crew import FinancialAnalystCrew
 
-
 def run():
-    inputs = {
-        "company_name": "Tesla",
-    }
-    FinancialAnalystCrew().crew().kickoff(inputs=inputs)
-
+    company_names = ["Tesla", "Apple"]
+    for company_name in company_names:
+        inputs = {
+            "company_name": company_name,
+        }
+        FinancialAnalystCrew().crew().kickoff(inputs=inputs)
 
 if __name__ == "__main__":
     run()
