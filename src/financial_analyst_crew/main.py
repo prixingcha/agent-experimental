b5bacd598f903c9687a44359c3792f6112615401
prixingcha <pratik.np@gmail.com>
2024-04-16 20:10:44 -0400

    fully working using python not with poetry

--- src/financial_analyst_crew/main.py
+++ src/financial_analyst_crew/main.py
@@ -1,11 +1,19 @@
+# import sys
+# sys.path.append('src')
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 from crew import FinancialAnalystCrew
+
 
 def run():
     inputs = {
-        'company_name': 'Tesla',
+        "company_name": "Tesla",
     }
     FinancialAnalystCrew().crew().kickoff(inputs=inputs)
+
 
 if __name__ == "__main__":
     run()
